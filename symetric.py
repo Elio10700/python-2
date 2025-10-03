820d8247ae8abfcfd39fcfa2aef682a5898fc92b
Elio <ebamah548@gmail.com>
2025-10-03 16:46:44 +0100

    Add files via upload

---
+++ symetric.py
@@ -0,0 +1,16 @@
+# Program to find the symmetric difference between two sets
+
+# define two sets
+set1 = {1, 2, 3, 4, 5}
+set2 = {4, 5, 6, 7, 8}
+
+# symmetric difference using ^ operator
+sym_diff = set1 ^ set2
+
+# or using symmetric_difference() method
+# sym_diff = set1.symmetric_difference(set2)
+
+print("Set 1:", set1)
+print("Set 2:", set2)
+print("The symmetric difference between set1 and set2 is:", sym_diff)
+    
